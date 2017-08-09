2e4903ff5033f4035f3b8cb278b7460f20c219d3
Giuseppe Bertone <bertone.giuseppe@gmail.com>
2017-08-09 20:36:40 +0200

    A little bit of formatting; Renamed event's return value;

--- contracts/Battle.sol
+++ contracts/Battle.sol
@@ -7,38 +7,42 @@ import "./SafeMathLib.sol";
 contract Battle is Timed {
     using SafeMathLib for uint256;
 
-    // Battle attributes
-    mapping (address => uint256) public warriorsOnTheBattlefield;       // Total troops fighting in the battle
-    mapping (address => uint256) public warriorsIntoAde;                // Total casualties, increased after each warriors redeem
-    mapping (address => mapping (address => uint256)) public warriors;  // Troops currently allocated by each player
-    
-    uint256 public constant maxPersians = 300000 * 10**18;  // 300.000
-    uint256 public constant maxSpartans = 300 * 10**18;     // 300
-    uint256 public constant maxImmortals = 100;             // 100
-    uint256 public constant maxAthenians = 100 * 10*18;     // 100
+    uint256  constant public maxPersians         = 300000 * 10**18;  // 300.000
+    uint256 public constant maxSpartans         = 300 * 10**18;     // 300
+    uint256 public constant maxImmortals        = 100;              // 100
+    uint256 public constant maxAthenians        = 100 * 10*18;      // 100
 
-    uint8 public constant battlePointDecimals = 18;
-    string public constant version = '1.0.0';
+    uint8   public constant battlePointDecimals = 18;
 
-    // Token addresses
-    address public constant persians = 0xF63D4Ec615898163CdE5EBC2Aa8d5768ccC92965;  //0xaec98a708810414878c3bcdf46aad31ded4a4557 MainNet
-    address public constant immortals = 0xED19698C0abdE8635413aE7AD7224DF6ee30bF22;  //0xED19698C0abdE8635413aE7AD7224DF6ee30bF22 MainNet
-    address public constant spartans = 0x94F72fa9d9e035a40F22f6e7ceDb78E697fF54C2;  //0x163733bcc28dbf26B41a8CfA83e369b5B3af741b MainNet
-    address public constant athenians = 0x17052d51E954592C1046320c2371AbaB6C73Ef10; //0x17052d51e954592c1046320c2371abab6c73ef10 MainNet
+    string  public constant version             = '1.0.0';
 
-    // Battle events
-    event WarriorsAssignedToBattlefield(address indexed from, address faction, uint256 battlePointsIncrementForecast);
-    event WarriorsBackToHome(address indexed to, address faction, uint256 warriors);
+    address public constant persians            = 0xF63D4Ec615898163CdE5EBC2Aa8d5768ccC92965;   //0xaec98a708810414878c3bcdf46aad31ded4a4557 MainNet
+    address public constant immortals           = 0xED19698C0abdE8635413aE7AD7224DF6ee30bF22;   //0xED19698C0abdE8635413aE7AD7224DF6ee30bF22 MainNet
+    address public constant spartans            = 0x94F72fa9d9e035a40F22f6e7ceDb78E697fF54C2;   //0x163733bcc28dbf26B41a8CfA83e369b5B3af741b MainNet
+    address public constant athenians           = 0x17052d51E954592C1046320c2371AbaB6C73Ef10;   //0x17052d51e954592c1046320c2371abab6c73ef10 MainNet
+
+    mapping (address => mapping (address => uint256))   public  warriors;                       // Troops currently allocated by each player
+    mapping (address => uint256)                        public  warriorsOnTheBattlefield;       // Total troops fighting in the battle
+    mapping (address => uint256)                        public  warriorsIntoAde;                // Total casualties, increased after each warriors redeem
+
+    event WarriorsAssignedToBattlefield (address indexed from, address faction, uint256 battlePointsIncrementForecast);
+    event WarriorsBackToHome            (address indexed to, address faction, uint256 survivedWarriors);
 
     /*******************************************************************************************
     When the battle ends in a draw:
         (*) 10% of troops of both sides lie on the ground
-        (*) 90% of them will be sent back to the former owner
+        (*) 90% of them can be retrieved by each former owner
         (*) No slaves are assigned
-    When the battle ends with a winner:
+    
+    When the battle ends with a winning factions:
         (*) 10% of troops of both sides lie on the ground
-        (*) 90% of them will be sent back to the former owner
-        (*) Surviving slaves are assigned to winner
+        (*) 90% of them can be retrieved by each former owner
+        (*) Surviving warriors of the loosing faction are assigned as slaves to winners
+    
+    Immortals and Athenians are support troops: there will be no casualties in their row, and they can
+    be retrieved without losses.
+    
+    Only Persians and Spartans can be slaves. Immortals and Athenians WILL NOT be sent back as slaves to winners.
     *******************************************************************************************/
     function Battle(uint256 _startTime, uint256 _life, uint8 _avarageBlockTime) Timed(_startTime, _life, _avarageBlockTime) { }
 
