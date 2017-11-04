054d524b4e876c31efe009383199ce900c11611d
Giuseppe Bertone <bertone.giuseppe@gmail.com>
2017-11-03 20:43:46 -0500

    Improved Upgradable contract;

--- contracts/Upgradable.sol
+++ contracts/Upgradable.sol
@@ -4,7 +4,7 @@ import "./Owned.sol";
 
 /**
  * The contract can be deprecated and the owner can set - only once - another address to advertise
- * to clients the existence of another more recent contract.
+ * clients of the existence of another more recent contract.
  */
 contract Upgradable is Owned {
 
@@ -12,16 +12,20 @@ contract Upgradable is Owned {
     bool    public deprecated;
     string  public newVersion;
     address public newAddress;
+    uint    public timestamp;
 
-    function Upgradable(string _version) {
+    function Upgradable(string _version)  {
         VERSION = _version;
     }
 
-    function setDeprecated(string _newVersion, address _newAddress) onlyOwner returns (bool success) {
-        require(!deprecated);
+    function setDeprecated(string _newVersion, address _newAddress) external onlyOwner returns (bool success) {
+        require(!deprecated && !Upgradable(_newAddress).deprecated());
+        address _currentAddress = this;
+        require(_newAddress != _currentAddress);
         deprecated = true;
         newVersion = _newVersion;
         newAddress = _newAddress;
+        timestamp = block.timestamp;
         return true;
     }
 }
